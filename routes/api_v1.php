ebab5f60e0010b5fc84a9febf298ea0bda96229e
Samir Elzer <76245712+samiralaa@users.noreply.github.com>
2024-10-14 11:45:33 +0300

    add new route file

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -7,3 +7,15 @@ Route::get('get-jobs',function()
 {
     dd('get-jobs');
 });
+Route::post('add-job',function(Request $request)
+{
+    dd('add-job');
+});
+Route::put('update-job',function(Request $request)
+{
+    dd('update-job');
+});
+Route::delete('delete-job',function(Request $request)
+{
+    dd('delete-job');
+});
